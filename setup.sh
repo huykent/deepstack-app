51096b41d908d9fc9885cca625d3a51197415fff
huykent <huykent.1989@gmail.com>
2024-09-03 23:50:52 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Xóa các tệp tin hoặc thư mục cũ
+rm -rf deepstack-app
+
+# Clone repository từ GitHub
+git clone https://github.com/huykent/deepstack-app.git
+
+# Chuyển đến thư mục của repository
+cd deepstack-app
+
+# Cấp quyền thực thi cho script cài đặt
+chmod +x install.sh
+
+# Chạy script cài đặt
+./install.sh
